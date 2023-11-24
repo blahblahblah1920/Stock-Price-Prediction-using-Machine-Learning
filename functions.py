b14325634811219bb4bf4d52e20663eb7a1fa566
blahblahblah1920 <pranavabishaimoses@gmail.com>
2023-11-23 22:32:13 -0600

    >>>

--- functions.py
+++ functions.py
@@ -6,6 +6,7 @@ from sklearn.metrics import mean_squared_error as mse
 import pandas as pd
 import numpy as np
 import streamlit as st
+import plotly.tools
 
 #funtion to plot the plots :D :
 def plot(aap,valid,m_name,s_name):
